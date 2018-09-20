2d75e5eee7ca6e64c3cae51050b7b09c88742f15
benfarice <imzoughene@outlook.com>
2018-09-20 14:44:50 +0100

    create fresh tomatoes movies trailers website

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,15 +1,18 @@
 import media
+import fresh_tomatoes
 toy_story = media.Movie(
 	"Toy Story",
 	"A story of a boy and his toys that come to life",
 	"https://i.ytimg.com/vi/rhHf_GdJwnE/maxresdefault.jpg",
 	"https://youtu.be/kATywRaquhA"
 	)
-print(toy_story.storyline)
+#print(toy_story.storyline)
 avatar = media.Movie(
 	"Avatar",
 	"A marine on an alien planet",
 	"https://i.ytimg.com/vi/rhHf_GdJwnE/maxresdefault.jpg",
 	"https://youtu.be/kATywRaquhA"
 	)
-avatar.show_trailer()
+#avatar.show_trailer()
+movies=[toy_story,avatar]
+fresh_tomatoes.open_movies_page(movies)
